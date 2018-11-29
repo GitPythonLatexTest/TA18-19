05fb788bf65363d1f98b3370bd9dca906c798971
faisal <ichaltkj96@gmail.com>
2018-11-29 18:51:52 +0700

    python.py

---
+++ python.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as faisal
+faisal.bar(['Mechanical','Piping','Instrument','Electrical','Civil','Safety'],[11,11,21,20,7,5])
+faisal.ylabel('Jumalah Barang')
+faisal.xlabel('Jenis Barang')
+faisal.show()
