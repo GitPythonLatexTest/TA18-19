f41fcc502179be7d24af535c3aa12dbfa1c64fa8
CAHY4KURNIAWAN <32796207+CAHY4KURNIAWAN@users.noreply.github.com>
2018-12-07 10:59:39 +0700

    Update cahya.py

--- cahya.py
+++ cahya.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x,y = np.genfromtxt('E:\INTERNSHIP\python\desaa.csv', unpack=True, delimiter=',')
+x,y = np.genfromtxt('E:\INTERNSHIP\python\cahya.csv', unpack=True, delimiter=',')
 plt.plot(x,y)
 plt.xlabel('desa')
 plt.ylabel('jumlah data')
